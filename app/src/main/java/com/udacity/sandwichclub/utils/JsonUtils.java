a9def9677e851f177242deda761244e478227f3f
Konstantinos Zafeiropoulos <k.a.zafeiropoulos@gmail.com>
2019-03-20 00:04:58 +0200

    Designed the layout for the detail activity so the different elements display in a sensible way. Implement the JSON parsing in JsonUtils so it produces a Sandwich Object that can be used to populate the UI that you designed.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,64 @@
 package com.udacity.sandwichclub.utils;
 
+
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        try {
+            JSONObject sandwichJson = new JSONObject(json);
+
+            JSONObject mName = sandwichJson.getJSONObject("name");
+            JSONArray mIngredients = sandwichJson.getJSONArray("ingredients");
+
+            String mainName = mName.getString("mainName");
+            String placeOfOrigin = sandwichJson.getString("placeOfOrigin");
+            String description = sandwichJson.getString("description");
+            String image = sandwichJson.getString("image");
+
+            List<String> alsoKnownAs = new ArrayList<>();
+            JSONArray jsonArrayWithAlsoKnownAsValues = mName.getJSONArray("alsoKnownAs");
+
+            if(jsonArrayWithAlsoKnownAsValues != null){
+                for(int i=0; i < jsonArrayWithAlsoKnownAsValues.length(); i++){
+                    String akaValue = jsonArrayWithAlsoKnownAsValues.getString(i);
+                    try{
+                        alsoKnownAs.add(akaValue);
+                    }catch (Exception e){
+                        Log.d(TAG, "parseSandwichJson: " + e.toString() );
+                    }
+
+                }
+            }
+
+            List<String> ingredients = new ArrayList<>();
+
+            if(mIngredients != null){
+                for(int i=0; i < mIngredients.length(); i++){
+                    String akaValue = mIngredients.getString(i);
+                    ingredients.add(akaValue);
+                }
+            }
+
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 }
